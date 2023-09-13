70f22202da3c0c0b69442beffb299902b1e5d3b3
SHUBHAM GUPTA <gshubham804@gmail.com>
2023-09-13 07:44:56 +0530

    form Ui changed

--- src/Pages/SharableForm.jsx
+++ src/Pages/SharableForm.jsx
@@ -1,240 +1,245 @@
 import  { useState } from 'react';
-import {FaLink} from 'react-icons/fa';
 
-function ProfessionalForm() {
-    const [formData, setFormData] = useState({
-        linkedin: '',
-        github: '',
-        twitter: '',
-        designation: '',
-        marketSize: '',
-        traction: '',
-        ask: '',
-        isRegistered: '',
-        useOfFunds: '',
-        competitiveLandscape: '',
-        businessModel: '',
-        goToMarketStrategy: '',
-        targetAudience: '',
-        solution: '',
-    });
+const CombinedForm = () => {
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState({
+    name: '',
+    address: '',
+    qualification: '',
+  });
+  const [teamMembers, setTeamMembers] = useState([
+    {
+      name: '',
+      designation: '',
+      linkedin: '',
+      github: '',
+      twitter: '',
+    },
+  ]);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+  const nextStep = () => {
+    setStep(step + 1);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // You can handle form submission here
-        console.log(formData);
-    };
+  const prevStep = () => {
+    setStep(step - 1);
+  };
 
-    return (
-        <div className="max-w-md mx-auto bg-white p-8 rounded shadow-md">
-            <div className='flex items-center justify-between '>
-            <h1 className="text-2xl font-semibold mb-4">Share this form with startup</h1>
-<FaLink className='text-blue-500 -mt-2 cursor-pointer'/>
+  const handleChange = (e, index) => {
+    const { name, value } = e.target;
+
+    if (step === 1) {
+      const updatedTeamMembers = [...teamMembers];
+      updatedTeamMembers[index][name] = value;
+      setTeamMembers(updatedTeamMembers);
+    } else if (step === 2) {
+      setFormData({ ...formData, [name]: value });
+    }
+  };
+
+  const addTeamMember = () => {
+    setTeamMembers([...teamMembers, { name: '', designation: '', linkedin: '', github: '', twitter: '' }]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (step < 8) {
+      nextStep();
+    } else {
+      // You can access both formData and teamMembers here and perform any desired action, such as submitting the data to an API.
+      console.log('Form Data:', formData);
+      console.log('Team Members:', teamMembers);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-96 bg-white shadow-lg rounded-lg p-6">
+        <form onSubmit={handleSubmit}>
+          {step === 1 && (
+            <div>
+              <h2 className="text-2xl font-medium mb-4">Team Members</h2>
+              {teamMembers.map((teamMember, index) => (
+                <div key={index} className="mb-4 bg-gray-50 p-4 rounded-lg shadow-md">
+                  <h2 className="text-lg font-medium mb-2">Team Member {index + 1}</h2>
+                  <div className="mb-2">
+                    <label className="block text-sm font-medium">Name:</label>
+                    <input
+                      type="text"
+                      name="name"
+                      value={teamMember.name}
+                      onChange={(e) => handleChange(e, index)}
+                      className="w-full p-2 border rounded focus:outline-none  focus:border-blue-300"
+                    />
+                  </div>
+                  <div className="mb-2">
+                    <label className="block text-sm font-medium">Designation:</label>
+                    <input
+                      type="text"
+                      name="designation"
+                      value={teamMember.designation}
+                      onChange={(e) => handleChange(e, index)}
+                      className="w-full p-2 border rounded focus:outline-none  focus:border-blue-300"
+                    />
+                  </div>
+                  <div className="mb-2">
+                    <label className="block text-sm font-medium">LinkedIn:</label>
+                    <input
+                      type="text"
+                      name="linkedin"
+                      value={teamMember.linkedin}
+                      onChange={(e) => handleChange(e, index)}
+                      className="w-full p-2 border rounded focus:outline-none  focus:border-blue-300"
+                    />
+                  </div>
+                  <div className="mb-2">
+                    <label className="block text-sm font-medium">GitHub:</label>
+                    <input
+                      type="text"
+                      name="github"
+                      value={teamMember.github}
+                      onChange={(e) => handleChange(e, index)}
+                      className="w-full p-2 border rounded focus:outline-none  focus:border-blue-300"
+                    />
+                  </div>
+                  <div>
+                    <label className="block text-sm font-medium">Twitter:</label>
+                    <input
+                      type="text"
+                      name="twitter"
+                      value={teamMember.twitter}
+                      onChange={(e) => handleChange(e, index)}
+                      className="w-full p-2 border rounded focus:outline-none  focus:border-blue-300"
+                    />
+                  </div>
+                </div>
+              ))}
+              <button
+                type="button"
+                onClick={addTeamMember}
+                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
+              >
+                Add Team Member
+              </button>
             </div>
-            <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-                    <label htmlFor="foundersname" className="block text-gray-700 font-medium"> 
-                    Founder's Name
-                        </label>
-                    <input
-                        type="text"
-                        id="foundersname"
-                        name="foundersname"
-                        value={formData.linkedin}
-                        onChange={handleChange}
-                        className="mt-1 p-2 w-full border rounded-md"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="linkedin" className="block text-gray-700 font-medium">LinkedIn URL</label>
-                    <input
-                        type="text"
-                        id="linkedin"
-                        name="linkedin"
-                        value={formData.linkedin}
-                        onChange={handleChange}
-                        className="mt-1 p-2 w-full border rounded-md"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="github" className="block text-gray-700 font-medium">GitHub URL</label>
-                    <input
-                        type="text"
-                        id="github"
-                        name="github"
-                        value={formData.github}
-                        onChange={handleChange}
-                        className="mt-1 p-2 w-full border rounded-md"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="twitter" className="block text-gray-700 font-medium">Twitter</label>
-                    <input
-                        type="text"
-                        id="twitter"
-                        name="twitter"
-                        value={formData.twitter}
-                        onChange={handleChange}
-                        className="mt-1 p-2 w-full border rounded-md"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="designation" className="block text-gray-700 font-medium">Designation</label>
-                    <input
-                        type="text"
-                        id="designation"
-                        name="designation"
-                        value={formData.designation}
-                        onChange={handleChange}
-                        className="mt-1 p-2 w-full border rounded-md"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="marketSize" className="block text-gray-700 font-medium">Market Size</label>
-                    <input
-                        type="text"
-                        id="marketSize"
-                        name="marketSize"
-                        value={formData.marketSize}
-                        onChange={handleChange}
-                        className="mt-1 p-2 w-full border rounded-md"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="traction" className="block text-gray-700 font-medium">Traction</label>
-                    <input
-                        type="text"
-                        id="traction"
-                        name="traction"
-                        value={formData.traction}
-                        onChange={handleChange}
-                        className="mt-1 p-2 w-full border rounded-md"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="ask" className="block text-gray-700 font-medium">Ask</label>
-                    <input
-                        type="text"
-                        id="ask"
-                        name="ask"
-                        value={formData.ask}
-                        onChange={handleChange}
-                        className="mt-1 p-2 w-full border rounded-md"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 font-medium">Is the Company Registered?</label>
-                    <div className="flex items-center space-x-4">
-                        <input
-                            type="radio"
-                            id="registered-yes"
-                            name="isRegistered"
-                            value="yes"
-                            checked={formData.isRegistered === 'yes'}
-                            onChange={handleChange}
-                        />
-                        <label htmlFor="registered-yes">Yes</label>
-                        <input
-                            type="radio"
-                            id="registered-no"
-                            name="isRegistered"
-                            value="no"
-                            checked={formData.isRegistered === 'no'}
-                            onChange={handleChange}
-                        />
-                        <label htmlFor="registered-no">No</label>
-                    </div>
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="useOfFunds" className="block text-gray-700 font-medium">
-                      Use of Funds</label>
-                    <textarea
-                        id="useOfFunds"
-                        name="useOfFunds"
-                        value={formData.useOfFunds}
-                        onChange={handleChange}
-                        rows="3"
-                        className="mt-1 p-2 w-full border rounded-md"
-                    ></textarea>
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="competitiveLandscape" className="block text-gray-700 font-medium">Competitive Landscape</label>
-                    <textarea
-                        id="competitiveLandscape"
-                        name="competitiveLandscape"
-                        value={formData.competitiveLandscape}
-                        onChange={handleChange}
-                        rows="3"
-                        className="mt-1 p-2 w-full border rounded-md"
-                    ></textarea>
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="businessModel" className="block text-gray-700 font-medium">Business Model</label>
-                    <textarea
-                        id="businessModel"
-                        name="businessModel"
-                        value={formData.businessModel}
-                        onChange={handleChange}
-                        rows="3"
-                        className="mt-1 p-2 w-full border rounded-md"
-                    ></textarea>
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="goToMarketStrategy" className="block text-gray-700 font-medium">
-                      Go-to Market Strategy</label>
-                    <textarea
-                        id="goToMarketStrategy"
-                        name="goToMarketStrategy"
-                        value={formData.goToMarketStrategy}
-                        onChange={handleChange}
-                        rows="3"
-                        className="mt-1 p-2 w-full border rounded-md"
-                    ></textarea>
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="targetAudience" className="block text-gray-700 font-medium">Target Audience</label>
-                    <textarea
-                        id="targetAudience"
-                        name="targetAudience"
-                        value={formData.targetAudience}
-                        onChange={handleChange}
-                        rows="3"
-                        className="mt-1 p-2 w-full border rounded-md"
-                    ></textarea>
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="solution" className="block text-gray-700 font-medium">
-                      Solution</label>
-                    <textarea
-                        id="solution"
-                        name="solution"
-                        value={formData.solution}
-                        onChange={handleChange}
-                        rows="3"
-                        className="mt-1 p-2 w-full border rounded-md"
-                    ></textarea>
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="product" className="block text-gray-700 font-medium">
-                      Product</label>
-                    <textarea
-                        id="product"
-                        name="product"
-                        value={formData.solution}
-                        onChange={handleChange}
-                        rows="3"
-                        className="mt-1 p-2 w-full border rounded-md"
-                    ></textarea>
-                </div>
-                
-            </form>
-        </div>
-    );
-}
+          )}
 
-export default ProfessionalForm;
+          {step === 2 && 
+            <div>
+              <div className="mb-2">
+                <label className="block text-base mb-2 font-medium">Problem Statement:</label>
+                <textarea
+                rows={5}
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={(e) => handleChange(e)}
+                  className="w-full p-2 border rounded focus:outline-none  focus:border-gray-500"
+                />
+              </div>
+              </div>
+          }
+          {step==3 && 
+           <div className="mb-2">
+           <label className="block text-base mb-2 font-medium">Solution:</label>
+           <textarea
+           rows={5}
+             type="text"
+             name="address"
+             value={formData.address}
+             onChange={(e) => handleChange(e)}
+             className="w-full p-2 border rounded focus:outline-none  focus:border-blue-300"
+           />
+         </div> }
+         {step==4 && 
+              <div>
+                <label className="block text-base mb-2 font-medium">
+                    Which product/service are you building?
+                </label>
+                <input
+                  type="text"
+                  name="qualification"
+                  value={formData.qualification}
+                  onChange={(e) => handleChange(e)}
+                  className="w-full p-2 border rounded focus:outline-none  focus:border-blue-300"
+                />
+            </div>
+          }
+
+{step==5 && 
+              <div>
+                <label className="block text-base mb-2 font-medium">Select market Startegy</label>
+                <input
+                  type="text"
+                  name="qualification"
+                  value={formData.qualification}
+                  onChange={(e) => handleChange(e)}
+                  className="w-full p-2 border rounded focus:outline-none  focus:border-blue-300"
+                />
+            </div>
+          }
+
+
+{step==6 && 
+              <div>
+                <label className="block text-base mb-2 font-medium">Enlist your competitors</label>
+                <input
+                  type="text"
+                  name="qualification"
+                  value={formData.qualification}
+                  onChange={(e) => handleChange(e)}
+                  className="w-full p-2 border rounded focus:outline-none  focus:border-blue-300"
+                />
+            </div>
+          }
+
+{step==7 && 
+              <div>
+                <label className="block text-base mb-2 font-medium">Is your company registered?</label>
+                <input
+                  type="text"
+                  name="qualification"
+                  value={formData.qualification}
+                  onChange={(e) => handleChange(e)}
+                  className="w-full p-2 border rounded focus:outline-none  focus:border-blue-300"
+                />
+            </div>
+          }
+
+{step==8 && 
+              <div>
+                <label className="block text-base mb-2 font-medium">If yes, please share registration no.</label>
+                <input
+                  type="text"
+                  name="qualification"
+                  value={formData.qualification}
+                  onChange={(e) => handleChange(e)}
+                  className="w-full p-2 border rounded focus:outline-none  focus:border-blue-300"
+                />
+            </div>
+          }
+
+          <div className="mt-6 flex justify-between">
+            {step > 1 && (
+              <button
+                type="button"
+                onClick={prevStep}
+                className="px-4 py-2 text-gray-600 rounded hover:bg-gray-200 focus:outline-none"
+              >
+                Back
+              </button>
+            )}
+            <button
+              type="submit"
+              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
+            >
+              {step ==8  ? 'Submit' : 'Next'}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CombinedForm;
