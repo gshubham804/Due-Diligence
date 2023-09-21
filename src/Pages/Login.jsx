38bab00285143d48684495a29516c9864748034b
SHUBHAM GUPTA <gshubham804@gmail.com>
2023-09-21 15:18:55 +0530

    backend integrated

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,81 +1,136 @@
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Logo1 from "../assets/1.png";
-
+const baseUrl = import.meta.env.VITE_REACT_BASE_URL;
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+  const { email, password } = formData;
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Make API request here to handle login
+      const response = await fetch(baseUrl+"auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ officialEmail:email, password }),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        // Successful login, you can handle the token here
+        navigate("/home");
+      } else {
+        alert("Invalid email or password!");
+        return;
+        // console.error(data.message);
+      }
+    } catch (error) {
+      // console.error(error.message);
+    }
+  };
+
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
-    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a href="#" className="flex items-center mb-8 text-2xl font-semibold
-       text-gray-900 dark:text-white">
-      <img
-  className=" mr-2"
-  src={Logo1}
-  alt="logo"
-  style={{ width: "240px", height: "50px" }} // Adjust the width and height as needed
-/>
-      </a>
-      <div className="w-full bg-white rounded-lg shadow
-       dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-        <div className="px-6 space-y-4 md:space-y-6 sm:px-8">
-          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-            Sign in to your account
-          </h1>
-          <form className="space-y-4 md:space-y-6" action="#">
-            <div>
-              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="name@company.com"
-                required=""
-              />
-            </div>
-            <div>
-              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="••••••••"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required=""
-              />
-            </div>
-            <div className="flex items-center justify-between">
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
-                  <input
-                    id="remember"
-                    aria-describedby="remember"
-                    type="checkbox"
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    required=""
-                  />
-                </div>
-                <div className="ml-3 text-sm">
-                  <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
-                </div>
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <Link to="/">
+          <a href="#" className="flex items-center mb-8 text-2xl font-semibold text-gray-900 dark:text-white">
+            <img
+              className="mr-2"
+              src={Logo1}
+              alt="logo"
+              style={{ width: "240px", height: "50px" }}
+            />
+          </a>
+        </Link>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="px-6 space-y-4 md:space-y-6 sm:px-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign in to your account
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={email}
+                  onChange={handleChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required
+                />
               </div>
-              <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
-            </div>
-        <div className="mt-6">
-            <Link to="/home">
-                    <button type="submit" className="w-full px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600">
-                        Login
-                    </button>
-                    </Link>
+              <div>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={password}
+                  onChange={handleChange}
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+              {/* <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                      required
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">
+                      Remember me
+                    </label>
+                  </div>
                 </div>
-            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-              Don’t have an account yet? <a href="/registration" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-            </p>
-          </form>
+                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">
+                  Forgot password?
+                </a>
+              </div> */}
+              <div className="mt-6">
+                <button
+                  type="submit"
+                  className="w-full px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600"
+                >
+                  Login
+                </button>
+              </div>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don’t have an account yet?{" "}
+                <Link to="/registration" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                  Sign up
+                </Link>
+              </p>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-  )
-}
+    </section>
+  );
+};
 
-export default Login
+export default Login;
--- src/Pages/Registration.jsx
+++ src/Pages/Registration.jsx
@@ -1,84 +1,162 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const baseUrl = import.meta.env.VITE_REACT_BASE_URL;
 
-function Registration() {
+const Registration = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    ventureFirmName: "",
+    website: "",
+    officialEmail: "",
+    password: "",
+  });
+
+  const navigate=useNavigate(); 
+  const { firstName, lastName, ventureFirmName, website, officialEmail, password } = formData;
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Make API request here to handle registration
+      const response = await fetch(baseUrl+"auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          ventureFirmName,
+          website,
+          officialEmail,
+          password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        // Registration successful, you can handle the response here
+       navigate("/home")
+      } else {
+        alert("email is already used")
+        // console.error(data.message);
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <div className="bg-gray-100 min-h-screen flex items-center justify-center">
       <div className="bg-white p-8 rounded shadow-md w-96">
         <h2 className="text-2xl font-semibold mb-4">Registration Form</h2>
-        <div className="mb-4">
-          <label htmlFor="firstName" className="block text-gray-700 text-sm font-bold mb-2">
-            First Name
-          </label>
-          <input
-            type="text"
-            id="firstName"
-            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-            placeholder="Enter your First Name"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="lastName" className="block text-gray-700 text-sm font-bold mb-2">
-            Last Name
-          </label>
-          <input
-            type="text"
-            id="lastName"
-            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-            placeholder="Enter your Last Name"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="ventureFirm" className="block text-gray-700 text-sm font-bold mb-2">
-            Venture Firm Name
-          </label>
-          <input
-            type="text"
-            id="ventureFirm"
-            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-            placeholder="Enter your Venture Firm Name"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="website" className="block text-gray-700 text-sm font-bold mb-2">
-            Website
-          </label>
-          <input
-            type="text"
-            id="website"
-            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-            placeholder="Enter your Website URL"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
-            Official Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-            placeholder="Enter your Official Email"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-            placeholder="Enter your Password"
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 text-white hover:bg-blue-600 rounded-lg px-4 py-2 w-full font-semibold"
-        >
-          Register
-        </button>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="firstName" className="block text-gray-700 text-sm font-bold mb-2">
+              First Name
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+              placeholder="Enter your First Name"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="lastName" className="block text-gray-700 text-sm font-bold mb-2">
+              Last Name
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              value={lastName}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+              placeholder="Enter your Last Name"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="ventureFirmName" className="block text-gray-700 text-sm font-bold mb-2">
+              Venture Firm Name
+            </label>
+            <input
+              type="text"
+              id="ventureFirmName"
+              name="ventureFirmName"
+              value={ventureFirmName}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+              placeholder="Enter your Venture Firm Name"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="website" className="block text-gray-700 text-sm font-bold mb-2">
+              Website
+            </label>
+            <input
+              type="text"
+              id="website"
+              name="website"
+              value={website}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+              placeholder="Enter your Website URL"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="officialEmail" className="block text-gray-700 text-sm font-bold mb-2">
+              Official Email
+            </label>
+            <input
+              type="email"
+              id="officialEmail"
+              name="officialEmail"
+              value={officialEmail}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+              placeholder="Enter your Official Email"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+              placeholder="Enter your Password"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white hover:bg-blue-600 rounded-lg px-4 py-2 w-full font-semibold"
+          >
+            Register
+          </button>
+        </form>
       </div>
     </div>
   );
-}
+};
 
 export default Registration;
