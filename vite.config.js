3798a05e101af181ad38e97da32ce7133ac5e41d
SHUBHAM GUPTA <gshubham804@gmail.com>
2023-09-21 20:17:57 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://dull-ox-cuff-links.cyclic.cloud', // Your backend server address
-        changeOrigin: true,
-      },
-    },
-  },
-});
+})
